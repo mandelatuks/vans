4927c401ccf75d788b70c9b4fd640412954e735a
mandelatuks <manderat206@gmail.com>
2023-04-06 00:19:20 +0200

    Change footer text

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 
 const Footer = () => {
-  return (
-    <div>Footer for the page</div>
-  )
+  return <footer>&#169; 2022 #VANLIFE</footer>;
 }
 
 export default Footer;
