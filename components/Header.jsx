537311563c953835f6e26ef8c896920a87a9b57b
mandelatuks <manderat206@gmail.com>
2023-04-07 22:46:20 +0200

    Adding style to the navbar

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,19 +1,40 @@
-import React from 'react';
-import { Link } from "react-router-dom";
- 
- const Header = () => {
-   return (
-     <header>
-       <Link className="site-logo" to="/">
-         #VanLife
-       </Link>
-       <nav>
-         <Link to="/host">Host</Link>
-         <Link to="/about">About</Link>
-         <Link to="/van">Vans</Link>
-       </nav>
-     </header>
-   );
- }
- 
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+const Header = () => {
+  const activeStyle = {
+    fontWeight: "bold",
+     textDecoration: "underline",
+     color: "#161616"
+}
+
+  return (
+    <header>
+      <Link className="site-logo" to="/">
+        #VanLife
+      </Link>
+      <nav>
+        <NavLink
+          to="/host"
+          style={({ isActive }) => (isActive ? activeStyle : null)}
+        >
+          Host
+        </NavLink>
+        <NavLink
+          to="/about"
+          style={({ isActive }) => (isActive ? activeStyle : null)}
+        >
+          About
+        </NavLink>
+        <NavLink
+          to="/van"
+          style={({ isActive }) => (isActive ? activeStyle : null)}
+        >
+          Vans
+        </NavLink>
+      </nav>
+    </header>
+  );
+};
+
 export default Header;
